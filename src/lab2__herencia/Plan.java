2523553b4a05897d320b7351efa4e2838670db3e
krismurillo22 <kristian_murillo@unitec.edu>
2024-07-26 13:28:42 -0600

    Creacion clase Plan -Avril

---
+++ src/lab2__herencia/Plan.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab2__herencia;
+
+/**
+ *
+ * @author User
+ */
+public abstract class Plan {
+    protected int numeroTelefono;
+    protected String nombreCliente;
+    
+    public Plan(String nombreCliente, int numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+        this.nombreCliente = nombreCliente;
+    }
+
+    public int getNumeroTelefono() {
+        return numeroTelefono;
+    }
+    
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+    
+    public abstract double pagoMensual(int mins, int msgs);
+
+    public String imprimir() {
+        return "Número de Teléfono: " + numeroTelefono + "\nNombre del Cliente:" + nombreCliente;
+    }
+}
